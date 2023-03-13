71bd177c561a51318cf8afe6fe84a25730ddbd13
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-13 13:57:11 +0200

    typo

--- create_admin_user.py
+++ create_admin_user.py
@@ -9,11 +9,11 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koodausblogi.settings')
     django.setup()
 
-    username=os.environ.get('ADMIN_USER_NAME', 'admin'),
-    email=os.environ.get('ADMIN_USER_EMAIL', ''),
-    password=os.environ['ADMIN_USER_PASSWORD']
+    username = os.environ.get('ADMIN_USER_NAME', 'admin')
+    password = os.environ['ADMIN_USER_PASSWORD']
+    email = os.environ.get('ADMIN_USER_EMAIL', '')
 
-    print(f"Creating superuser, {username!r}")
+    print(f"Creating superuser {username!r}")
     get_user_model().objects.create_superuser(username, email, password)
 
 if __name__ == '__main__':
