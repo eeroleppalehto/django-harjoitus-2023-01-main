59c094f72938d1cf35121f4b8894b6829a609592
eeroleppalehto <eero.leppalehto@gmail.com>
2023-03-13 10:59:32 +0200

    Secret key

--- koodausblogi/settings.py
+++ koodausblogi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vj+3uk4g3r#p%ljk8gvv4^3^j_&@zeqp3i#%5w!qheci8k8pk4'
+# SECRET_KEY = 'django-insecure-vj+3uk4g3r#p%ljk8gvv4^3^j_&@zeqp3i#%5w!qheci8k8pk4'
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='secret')
 
